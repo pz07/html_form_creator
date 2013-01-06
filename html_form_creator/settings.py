30a74dac0a36b42e67633f13b3590f075986fd46
Pawel Zieba <pawel.zieba@payu.pl>
2013-01-06 20:55:25 +0100

    context change for fcgi

--- html_form_creator/settings.py
+++ html_form_creator/settings.py
@@ -14,7 +14,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'sqlite3.db',                      # Or path to database file if using sqlite3.
+        'NAME': os.path.join(os.path.dirname(__file__), '../sqlite3.db'),                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -65,7 +65,7 @@ STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = '/static/html_form_creator/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
--- html_form_creator/urls.py
+++ html_form_creator/urls.py
@@ -7,10 +7,10 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^html_form_creator/', include('html_form_creator.foo.urls')),
 
-    url(r'^$', 'creator.views.templates', name='templates'),
-    url(r'^template/(?P<template_id>\d+)/forms$', 'creator.views.template'),
-    url(r'^template/(?P<template_id>\d+)/form/(?P<form_id>\d+)/form$', 'creator.views.form'),
-    url(r'^template/(?P<template_id>\d+)/form/(?P<form_id>\d+)/edit$', 'creator.views.form_edit'),
+    url(r'^html_form_creator/$', 'creator.views.templates', name='templates'),
+    url(r'^html_form_creator/template/(?P<template_id>\d+)/forms$', 'creator.views.template'),
+    url(r'^html_form_creator/template/(?P<template_id>\d+)/form/(?P<form_id>\d+)/form$', 'creator.views.form'),
+    url(r'^html_form_creator/template/(?P<template_id>\d+)/form/(?P<form_id>\d+)/edit$', 'creator.views.form_edit'),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^html_form_creator/admin/', include(admin.site.urls)),
 )
